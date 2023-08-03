9487246df4ee803978c472c06102e8ff12934d15
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-08-03 14:10:49 +0200

    removed major bugs, game playable

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  
- const Player = (symbol) =>{
+ const Player = (symbol) => {
+
     this.symbol = symbol;
 
     const getSymbol = () =>{
@@ -12,8 +13,10 @@
 
     return{getSymbol, setSymbol};
  };
+
  
  const GameBoard = () => {
+
     const board = ["","","","","","","","",""];
     const setField = (index, symbol) =>{
         if (index > board.length) return;
@@ -37,22 +40,19 @@
         console.log(board);
     }
 
-    const checkResult = () => {
-        if((board[0]==board[1] && board[1]==board[2] && board[0]!=="")||(board[3]==board[4] && board[4]==board[5] && board[3]!=="")||(board[6]==board[7] && board[7]==board[8] && board[6]!=="")
-            || (board[0]==board[3] && board[3]==board[6] && board[0]!=="")|| (board[1]==board[4] && board[4]==board[7] && board[1]!=="")||(board[2]==board[5] && board[5]==board[8] && board[2]!=="")
-            || (board[0]==board[4] && board[4]==board[8] && board[0]!=="")|| (board[2]==board[4] && board[4]==board[6] && board[2]!=="")){
-                console.log()
-            }
+    const getBoard = () => {
+        return board;
     }
 
-    return{ checkResult, setField, getField, clear, populateField};
+    return{getBoard, setField, getField, clear, populateField};
  };
 
 
 const GameController = () => {
+
     const board = GameBoard();
-    const player1 = Player("x");
-    const player2 = Player("o");
+    const player1 = Player("X");
+    const player2 = Player("O");
 
     let activePlayer = player1;
 
@@ -76,16 +76,19 @@ const GameController = () => {
     }
 
     const checkResult = () => {
-        if((board[0]==board[1] && board[1]==board[2] && board[0]!=="")||(board[3]==board[4] && board[4]==board[5] && board[3]!=="")||(board[6]==board[7] && board[7]==board[8] && board[6]!=="")
-            || (board[0]==board[3] && board[3]==board[6] && board[0]!=="")|| (board[1]==board[4] && board[4]==board[7] && board[1]!=="")||(board[2]==board[5] && board[5]==board[8] && board[2]!=="")
-            || (board[0]==board[4] && board[4]==board[8] && board[0]!=="")|| (board[2]==board[4] && board[4]==board[6] && board[2]!=="")){
-                console.log(activePlayer + 'won!');
+        let myBoard = board.getBoard();
+
+        if(((myBoard[0]==myBoard[1] && myBoard[1]==myBoard[2]) && myBoard[0]!=="")||((myBoard[3]==myBoard[4] && myBoard[4]==myBoard[5]) && myBoard[3]!=="")||((myBoard[6]==myBoard[7] && myBoard[7]==myBoard[8]) && myBoard[6]!=="")
+            || ((myBoard[0]==myBoard[3] && myBoard[3]==myBoard[6]) && myBoard[0]!=="")|| ((myBoard[1]==myBoard[4] && myBoard[4]==myBoard[7]) && myBoard[1]!=="")||((myBoard[2]==myBoard[5] && myBoard[5]==myBoard[8]) && myBoard[2]!=="")
+            || ((myBoard[0]==myBoard[4] && myBoard[4]==myBoard[8]) && myBoard[0]!=="")|| ((myBoard[2]==myBoard[4] && myBoard[4]==myBoard[6]) && myBoard[2]!=="")){
+                console.log(activePlayer.getSymbol() + ' won!');
             }
     }
 
     const clearBoard =() => {
         board.clear;
     }
+
     return {switchTurn, playRound, getActivePlayer, clearBoard, getBoard};
 };
  
@@ -110,10 +113,9 @@ const ScreenController = () => {
             cell.innerHTML = board.getField(index);
         })
     }
-        updateScreen();
 
-        return{updateScreen};
 }
+
 ScreenController();
 
 
